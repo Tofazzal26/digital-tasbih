76bb9f663a7ce6a1849fcad51a85445723e30908
Tofazzal26 <afranislamabir6789@gmail.com>
2024-02-11 21:50:34 +0600

    digital tasbhi design successfully complete

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const displayInputvalue = document.getElementById('displayInputvalue');
+const incrementSubhanAllah = document.getElementById('incrementSubhanAllah');
+const decrementSubhanAllah = document.getElementById('decrementSubhanAllah');
+
+
